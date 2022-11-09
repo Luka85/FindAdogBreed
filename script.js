c547ad54be5b3a45f7a16599ac622c3d87e89b50
Luka Kikelj <kikelj.luka@gmail.com>
2022-11-09 16:00:18 +0100

    add search by breeds and add input validation

--- script.js
+++ script.js
@@ -2,22 +2,59 @@
 
 const searchInput = document.querySelector(".search__input");
 const searchButton = document.querySelector(".search__button");
-const result = document.querySelector("ul");
+const resultData = document.querySelector("ul");
+let notification = document.createElement("li");
+let newLi;
+let data;
+const arrayOfBreeds = [];
 
 const getData = async () => {
   try {
     const resolve = await axios.get("https://api.thedogapi.com/v1/breeds");
-    const data = resolve.data;
+    data = resolve.data;
+    // console.log(data);
     for (let i = 0; i < data.length; i++) {
-      const newLi = document.createElement("li");
+      newLi = document.createElement("li");
+      newLi.classList.add("result");
       newLi.append(data[i].name);
-      result.append(newLi);
+      resultData.append(newLi);
+      arrayOfBreeds.push(data[i].name);
     }
   } catch (error) {
-    const notification = document.createElement("li");
-    notification.append("Nothing found. Please try again");
-    result.append(notification);
+    notification = document.createElement("li");
+    notification.append(
+      " Not found. Something went wrong. Please try again later"
+    );
+    resultData.append(notification);
   }
 };
 
+//FETCH THE DATA FROM THE API ON WINDOW LOAD WITH THE getData FUNCTION
 window.addEventListener("load", getData);
+
+//INPUT SEARCH WITH THE findBreed FUNCTION
+searchInput.addEventListener("input", findBreed);
+
+function findBreed() {
+  resultData.innerHTML = "";
+  notification = document.createElement("li");
+  for (let i = 0; i < arrayOfBreeds.length; i++) {
+    if (
+      arrayOfBreeds[i]
+        .toLowerCase()
+        .includes(searchInput.value.toLowerCase()) &&
+      !(searchInput.value === " ")
+    ) {
+      notification.style.display = "none";
+      newLi = document.createElement("li");
+      newLi.style.display = "block";
+      newLi.classList.add("result");
+      newLi.append(arrayOfBreeds[i], i);
+      resultData.append(newLi);
+    }
+
+    notification.classList.add("notification");
+    notification.textContent = "Nothing found. Please try again";
+    resultData.append(notification);
+  }
+}
