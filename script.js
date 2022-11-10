c4ab1298d524f4ba82a6b85a219a01a6616d6cb1
Luka Kikelj <kikelj.luka@gmail.com>
2022-11-10 12:08:44 +0100

    fix findBreed function

--- script.js
+++ script.js
@@ -5,20 +5,19 @@ const searchButton = document.querySelector(".search__button");
 const resultData = document.querySelector("ul");
 let notification = document.createElement("li");
 let newLi;
-let data;
-const arrayOfBreeds = [];
+let breedsData;
+let resolve;
 
 const getData = async () => {
   try {
-    const resolve = await axios.get("https://api.thedogapi.com/v1/breeds");
-    data = resolve.data;
-    // console.log(data);
-    for (let i = 0; i < data.length; i++) {
+    resolve = await axios.get(`https://api.thedogapi.com/v1/breeds/`);
+    breedsData = resolve.data;
+    // console.log(breedsData);
+    for (let i = 0; i < breedsData.length; i++) {
       newLi = document.createElement("li");
       newLi.classList.add("result");
-      newLi.append(data[i].name);
+      newLi.append(breedsData[i].name);
       resultData.append(newLi);
-      arrayOfBreeds.push(data[i].name);
     }
   } catch (error) {
     notification = document.createElement("li");
@@ -32,29 +31,39 @@ const getData = async () => {
 //FETCH THE DATA FROM THE API ON WINDOW LOAD WITH THE getData FUNCTION
 window.addEventListener("load", getData);
 
-//INPUT SEARCH WITH THE findBreed FUNCTION
-searchInput.addEventListener("input", findBreed);
-
-function findBreed() {
+//FETCH THE DATA BASED ON INPUT SEARCH AND INPUT VALIDATION
+const findBreed = async () => {
   resultData.innerHTML = "";
   notification = document.createElement("li");
-  for (let i = 0; i < arrayOfBreeds.length; i++) {
-    if (
-      arrayOfBreeds[i]
-        .toLowerCase()
-        .includes(searchInput.value.toLowerCase()) &&
-      !(searchInput.value === " ")
-    ) {
+  try {
+    resolve = await axios.get(
+      `https://api.thedogapi.com/v1/breeds/search?q=${searchInput.value}`
+    );
+    breedsData = resolve.data;
+    for (let i = 0; i < breedsData.length; i++) {
       notification.style.display = "none";
       newLi = document.createElement("li");
       newLi.style.display = "block";
       newLi.classList.add("result");
-      newLi.append(arrayOfBreeds[i], i);
+      newLi.append(breedsData[i].name);
       resultData.append(newLi);
+      searchInput.focus();
     }
-
+    if (breedsData.length === 0) {
+      searchInput.value = "";
+    }
     notification.classList.add("notification");
     notification.textContent = "Nothing found. Please try again";
     resultData.append(notification);
+    searchInput.focus();
+  } catch (error) {
+    notification = document.createElement("li");
+    notification.append(
+      "Not found. Something went wrong. Please try again later."
+    );
+    console.log(" Not found. Something went wrong. Please try again later.");
+    resultData.append(notification);
   }
-}
+};
+
+searchButton.addEventListener("click", findBreed);
